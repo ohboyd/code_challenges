212bda34bd4fd3c04cf2348013d410111e282e5a
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-14 01:37:56 -0400

    Create square root method

---
+++ 10_square_root.rb
@@ -0,0 +1,30 @@
+# Square Root
+
+# Implement a method to calculate square root of a given number.
+
+def sqrt(n)
+  n ** (0.5)
+end
+
+# happy path
+puts sqrt(4)
+puts sqrt(9)
+puts sqrt(25)
+puts sqrt(36)
+puts sqrt(49)
+
+# primes
+puts sqrt(2)
+puts sqrt(3)
+puts sqrt(5)
+puts sqrt(7)
+
+# zero
+puts sqrt(0)
+
+# negative
+puts sqrt(-4)
+puts sqrt(-9)
+puts sqrt(-25)
+puts sqrt(-36)
+puts sqrt(-49)
