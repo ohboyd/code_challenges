5bf29d59681d368b7062494169cf211bdc4bd2e4
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-15 21:47:25 -0400

    Recreate the adjacent elements formula to calculate new arrays

--- 08_add_adjacent_elements.rb
+++ 08_add_adjacent_elements.rb
@@ -1,22 +1,43 @@
 # Add Adjacent Elements
 
 # Add adjacent elements between 0 and n.
+# Here is a sample input and output:
 
-def adder_up(n)
-  return "Number must be positive" unless n > 0
+# Input  : 5, 10, 15, 20
+# Output : 15, 25, 35
 
-  return n if (n-1).zero?
+def sum_it_up(array)
+  return array if array.size == 1
 
-  n + adder_up(n-1)
+  new_array = []
+
+  array.size.times do |number|
+    if (number + 1) < array.size
+      new_array << (array[number] + array[number + 1])
+    end
+  end
+
+  new_array
 end
 
 # happy path
-puts adder_up(5)
-puts adder_up(69)
-puts adder_up(9999)
+puts sum_it_up([3, 4, 5, 6])
+puts sum_it_up([5, 10, 15, 20])
+puts sum_it_up([1, 4, 9, 16, 25])
 
-# zero
-puts adder_up(0)
+# negative numbers
+puts sum_it_up([-3, -4, -5, -6])
+puts sum_it_up([-5, -10, -15, -20])
 
-# negative
-puts adder_up(-369)
+# mixed signs (like my girlfriend...)
+puts sum_it_up([-3, 4, -5, 6])
+puts sum_it_up([5, -10, 15, -20])
+
+# one value
+puts sum_it_up([1])
+puts sum_it_up([69])
+puts sum_it_up([8_000_000])
+
+# no values
+puts sum_it_up([])
+puts sum_it_up([]).class
