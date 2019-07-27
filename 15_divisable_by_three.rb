15475c91b85238ff6b5bd02292064baab029fe4f
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-27 15:09:09 -0400

    Create method to see if a number is divisable by 3

---
+++ 15_divisable_by_three.rb
@@ -0,0 +1,25 @@
+# Divisible
+
+# Write a method to check if a given number is divisible by 3.
+
+def threesome(n)
+  return true if (n % 3).zero? && !n.zero?
+
+  false
+end
+
+# happy path
+puts threesome(3)
+puts threesome(6)
+puts threesome(9)
+puts threesome(7)
+puts threesome(99)
+
+# negative numbers
+puts threesome(-3)
+puts threesome(-6)
+puts threesome(-9)
+puts threesome(-7)
+
+# zero
+puts threesome(0)
