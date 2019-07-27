c3f912afed91a221c42144f268432aa46f446aed
Mike Boyd <mike.boyd@radicalbear.com>
2019-07-27 15:40:49 -0400

    Create a method to reverse a string

---
+++ 17_reverse_string.rb
@@ -0,0 +1,24 @@
+# Reverse String
+
+# Implement a method reverse that reverses a given string.
+
+def string_gnirts(word)
+  word.reverse
+end
+
+# happy path
+puts string_gnirts('test')
+puts string_gnirts('that')
+puts string_gnirts('given')
+puts string_gnirts('string')
+puts string_gnirts('is')
+puts string_gnirts('reversed')
+
+# with spaces
+puts string_gnirts('even if there are spaces')
+
+# with punctuation and spaces
+puts string_gnirts('or punctuation!!')
+
+
+puts string_gnirts('ysae tihs siht sekam ybur')
